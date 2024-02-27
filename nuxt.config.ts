0dae1d3522808242fcfbbb0eab4918b8bdc47e36
Hosein-NZF <HoseinnNazifi@gmail.com>
2024-02-27 16:50:59 -0500

    Update baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    baseURL: '/modern-admin-panel/'
+  },
   typescript: {
     shim: false
   },
